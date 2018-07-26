8723c708b7f8e7f1df7476229f581abf75f09fa9
Juan Manuel Mauricio <jmmauricio6@gmail.com>
2018-07-26 16:35:31 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+with open('README.rst') as f:
+    readme = f.read()
+
+setup(
+    name='pyvsc',
+    version='0.1',
+    description='VSC Losses Electrothermal Model',
+    long_description=readme,
+    author='Juan Manuel Mauricio',
+    author_email='jmmauricio@us.es',
+    packages=['pyvsc', 'pyvsc.tests'],
+    classifiers=[
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3',
+    ]
+)
